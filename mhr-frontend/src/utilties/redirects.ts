0cfa200bda2faf9142189cfd50f38ace410eef91
Jeremy Liguori <jeremyliguori@nyu.edu>
2025-03-06 17:55:17 -0800

    still redirects

--- mhr-frontend/src/utilties/redirects.ts
+++ mhr-frontend/src/utilties/redirects.ts
@@ -1,7 +1,7 @@
 import { config as payloadConfig } from 'mhr-payload'
 import { getPayload } from 'payload'
 
-export async function getRedirects(depth =1) {
+export async function getRedirects(depth = 1) {
     const payload = await getPayload({ config: payloadConfig })
 
     const { docs: redirects } = await payload.find({
@@ -12,5 +12,14 @@ export async function getRedirects(depth =1) {
     })
 
     // return redirects in astro format, ensuring the from slug includes a '/'
+    return redirects.reduce((acc, redirect) => {
+        const { from, to } = redirect
+        const formattedFrom = from.startsWith('/') ? from : `/${from}`
 
+        if (to?.type === 'reference') {
+            acc[formattedFrom] = to.reference?.value.slug
+        }
+        
+
+    }, {})
 }
