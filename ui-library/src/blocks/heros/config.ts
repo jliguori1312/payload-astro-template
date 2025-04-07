dcf063f3ed03392809514f8a1af91623084a2dcb
Jeremy Liguori <jeremyliguori@nyu.edu>
2025-04-06 20:42:24 -0700

    move linkGroup field

--- ui-library/src/blocks/heros/config.ts
+++ ui-library/src/blocks/heros/config.ts
@@ -7,7 +7,7 @@ import {
   lexicalEditor,
 } from '@payloadcms/richtext-lexical'
 
-import { linkGroup } from '@/fields/linkGroup'
+import { linkGroup } from '#util/fields/linkGroup'
 
 export const hero: Field = {
   name: 'hero',
