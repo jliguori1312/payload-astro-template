aa8e0d635c6ebb3f85b3a7be9b06ad1926cf78f6
Jeremy Liguori <jeremyliguori@nyu.edu>
2025-04-06 21:00:58 -0700

    move Content block

--- ui-library/src/blocks/Content/config.ts
+++ ui-library/src/blocks/Content/config.ts
@@ -7,7 +7,7 @@ import {
   lexicalEditor,
 } from '@payloadcms/richtext-lexical'
 
-import { link } from '@/fields/link'
+import { link } from '#util/fields/link'
 
 const columnFields: Field[] = [
   {
