749323dba1ddf2e4a4f82fc67b101e6e67d48635
Jeremy Liguori <jeremyliguori@nyu.edu>
2025-06-20 22:36:29 -0700

    working on loader

--- astro-payload-local-loader/payload-local-loader.ts
+++ astro-payload-local-loader/payload-local-loader.ts
@@ -1,14 +1,36 @@
 import type { Loader, LoaderContext } from 'astro/loaders'
-import type { SanitizedConfig } from 'payload'
+import { type SanitizedConfig, type RequiredDataFromCollectionSlug, getPayload } from 'payload'
 import { z } from 'astro:content'
 
 
-export function payloadLocalLoader(options: { payloadConfig: SanitizedConfig }): Loader {
-    // configure paylaod config
+export async function payloadLocalLoader(options: { payloadConfig: SanitizedConfig, draft: boolean }): Loader {
+    const payload = await getPayload({config: options.payloadConfig})
     return {
-        name: 'pload-local-loader',
-        load: async (context: LoaderContext): Promise<void> => {
+        name: 'payload-local-loader',
+        load: async ({collection, generateDigest, store, logger}): Promise<void> => {
             // get the payload collection and update the store
+            const data = await payload.find({
+                collection,
+                draft: options.draft,
+                limit: 1000,
+                overrideAccess: false,
+                pagination: false,
+            })
+
+            for (const page of data.docs) {
+                const digest = generateDigest(page)
+
+                store.set({
+                    id: String(page.id),
+                    digest,
+                    data: page
+                }) // it thinks page.id is maybe a number...
+                // check datastore for anything that doesn't exist and delete it
+                // see latest chatgpt
+                
+                // add schema for the basic payload structure, docs array, id, layout, etc
+                
+            }
         }
     }
 }
