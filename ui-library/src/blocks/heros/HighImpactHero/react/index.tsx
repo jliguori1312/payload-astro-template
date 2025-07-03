5c42b1a300a4751ad36eef3546ffaeb1b6351953
Jeremy Liguori <jeremyliguori@nyu.edu>
2025-07-02 21:20:57 -0700

    update heading styles and add prose

--- ui-library/src/blocks/heros/HighImpactHero/react/index.tsx
+++ ui-library/src/blocks/heros/HighImpactHero/react/index.tsx
@@ -30,7 +30,7 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
       </div>
       <div className="min-h-[80vh] select-none">
         {media && typeof media === 'object' && (
-          <Media fill imgClassName="-z-10 object-cover absolute h-full w-full inset-0" priority resource={media} />
+          <Media imgClassName="-z-10 object-cover absolute h-full w-full inset-0" priority resource={media} />
         )}
       </div>
     </div>
